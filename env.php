f9ebbc71380a0f590e03b74174f76ba9f21ce707
PWuttam <mito.takuwan@hotmail.co.jp>
2020-06-30 10:21:51 +0545

    ユーザーテーブルの作成

--- env.php
+++ env.php
@@ -1,9 +1,16 @@
 <?php
 
-// ここにパスワードとかもあるので env.phpはGitに上げない
+// ここにパスワードとかもあるので env.phpはGitに載せない
 
-// define 定数
+// define とは 定数
+// 記事の管理用
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'blog_app');
 define('DB_USER', 'blog_user');
 define('DB_PASS', 'takuya');
+
+// ログイン機能用
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'user');
+define('DB_USER', 'takuya');
+define('DB_PASS', 'takuya123');
